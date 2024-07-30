689455f7fa2594a46c4562320157460f4125af2d
Mateusz Swiderski <dzordzo221@gmail.com>
2024-07-30 20:27:26 +0100

    Vertical slices - HrTechnique Architecture Refactor 3/Addon [Fluent Api introduction, Grouping configuration]

--- HrTechniqueServer/Infrastructure/Persistence/Data/HrtBaseDataContext.cs
+++ HrTechniqueServer/Infrastructure/Persistence/Data/HrtBaseDataContext.cs
@@ -1,4 +1,5 @@
 ﻿using HrTechniqueServer.Domain.Entities;
+using HrTechniqueServer.Infrastructure.Persistence.Data.EntityTypeConfigurations;
 using Microsoft.EntityFrameworkCore;
 using File = HrTechniqueServer.Domain.Entities.File;
 
@@ -8,4 +9,9 @@ public class HrtBaseDataContext : DbContext
 {
     public DbSet<Employee> Employees { get; set; }
     public DbSet<File> Files { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        new EmployeeEntityTypeConfiguration().Configure(modelBuilder.Entity<Employee>());
+    }
 }
